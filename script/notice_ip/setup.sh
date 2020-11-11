c691766489393aee8d64e28f15877ce7e03c49e5
SilverW0o0W <silver.codingcat@gmail.com>
2020-11-11 20:32:16 +0800

    Update setup.sh

--- script/notice_ip/setup.sh
+++ script/notice_ip/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# cp notice_ip.service /etc/systemd/system/notice_ip.service
-pip install requirements.txt
+cp notice_ip.service /etc/systemd/system/notice_ip.service
+systemctl daemon-reload
 systemctl enable notice_ip.service
+pip install requirements.txt
