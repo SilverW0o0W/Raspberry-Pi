b108673d74873c79e186400d139378303a42145e
SilverW0o0W <silver.codingcat@gmail.com>
2020-11-14 19:12:51 +0800

    Update files

--- script/notice_ip/setup.sh
+++ script/notice_ip/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-cp notice_ip.service /lib/systemd/system/notice_ip.service
+SC_KEY=$1
+
+sc_key = "$SC_KEY" >> /etc/notice_ip/notice_ip.toml
+cp src/notice_ip.service /lib/systemd/system/notice_ip.service
+cp -r src/python /usr/local/src/notice_ip
+pip install --no-cache-dir -r requirements.txt
 systemctl daemon-reload
 systemctl enable notice_ip.service
-pip install --no-cache-dir -r requirements.txt
