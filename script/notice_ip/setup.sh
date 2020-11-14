6d9889805933fc64be4aed4c0e80ca71224548ac
SilverW0o0W <silver.codingcat@gmail.com>
2020-11-14 19:19:18 +0800

    Update setup.sh

--- script/notice_ip/setup.sh
+++ script/notice_ip/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SC_KEY=$1
 
-sc_key = "$SC_KEY" >> /etc/notice_ip/notice_ip.toml
+echo "sc_key = \"$SC_KEY\"" > /etc/notice_ip/notice_ip.toml
 cp src/notice_ip.service /lib/systemd/system/notice_ip.service
 cp -r src/python /usr/local/src/notice_ip
 pip install --no-cache-dir -r requirements.txt
